007302c541f3b768f170b4add2f1a284c83e047e
aquib-javed <aquib.j.basra@gmail.com>
2023-11-16 18:51:54 +0500

    Initial commit

---
+++ src/Component/navbar/navbar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <>
+        <object data="https://sarmaaya.pk/public/widgets/stocks-overview" width="100%" height="110" type="text/html"> Stocks Overview </object>
+            <nav className="bg-[#142941] h-16 text-[#D9ECFF] flex justify-between basis-3">
+                <a href="" className="flex items-center ml-8">
+                    <span >Trade</span>
+                    <span className="text-[#21B573]">Guru.pk</span>
+                </a>
+                <div className="flex items-center space-x-6">
+                    <button>Home</button>
+                    <button>course</button>
+                    <button>learn</button>
+                    <button>stocks</button>
+                    <button>Porfolio</button>
+                </div>
+                <div className="flex items-center space-x-4 mr-8 ">
+                    <button className="border rounded-md outline-2 w-20 border-[#D9ECFF]" >Login</button>
+                    <button className="border rounded-md outline-2 w-20  border-[#D9ECFF]">Signup</button>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+
+
+{/* <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+                        hello
+                    </a>
+
+           
+
+
+            <div className="bg-[#142941] h-screen">
+               <section className="bg-[#20344A]  w-96 h-96 rounded-lgr">
+                <div className="text-[#D9ECFF] flex flex-col flex items-center">
+                    <h1>login</h1>
+                    <input></input>
+                </div>
+
+
+
+
+*/}
---
+++ src/Component/signup/signup.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState } from "react";
+
+
+export default function Signup() {
+    return (
+      <>
+      <section className="bg-[#20344A] h-screen">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto shadow-lg">
+                   
+                    <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md ">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                                Login to your account
+                            </h1>
+                            <form className="space-y-4 md:space-y-6" action="#">
+                                <div>
+                                    <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@company.com" required="">
+                                    </input>
+                                </div>
+                                <div>
+                                    <label for="password" className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                                    
+                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required="">
+                                    </input>
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <div className="flex items-start">
+                                        <div className="flex items-center h-5">
+                                            <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                                            </input>
+                                        </div>
+                                        <div className="ml-3 text-sm">
+                                            <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                                        </div>
+                                    </div>
+                                    <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                                </div>
+                                
+                                <button type="submit" className="border rounded-md outline-2 border-[#D9ECFF] bg-[#20344A] text-[#D9ECFF] w-full px-5 py-2.5 text-center ">Sign in</button>
+                                <p className="text-sm font-light text-black ">
+                                    Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline">Sign up</a>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+      </>
+      );
+  }
