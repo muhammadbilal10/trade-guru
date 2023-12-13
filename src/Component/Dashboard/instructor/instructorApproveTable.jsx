939bd6e7ff1aefbcb34c26ce5228b2d120eee1db
aquib-javed <aquib.j.basra@gmail.com>
2023-12-13 22:48:04 +0500

    Create instructorApproveTable.jsx

---
+++ src/Component/Dashboard/instructor/instructorApproveTable.jsx
@@ -0,0 +1,249 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import app from "../../../database/firebase";
+import { fetchUserData } from '../js file/instructor';
+import { FaEdit } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
+import EditModal from './EditModal';
+import DeleteModal from './deletemodal';
+import { SourceCode } from 'eslint';
+
+export default function EditTable() {
+
+    const [IsEditOpen, setIsEditOpen] = useState(false)
+    const [IsDelOpen, setIsDelOpen] = useState(false)
+    const [user, setUser] = useState(null);
+    const [currentuser, setCurrentUser] = useState(null);
+    const [uid, setUid] = useState(null);
+    const db = getFirestore(app);
+
+    useEffect(() => {
+        // Function to get documents where status is false
+        const getDocumentsWithStatusFalse = async () => {
+            const myCollection = collection(db, 'Instructor');
+            const statusFalseQuery = query(myCollection, where('status', '==', false));
+
+            try {
+                const querySnapshot = await getDocs(statusFalseQuery);
+                const documents = querySnapshot.docs.map(doc => doc.data());
+                console.log('Documents with status false:', documents);
+                setUser(documents);
+                // Set the documents to the state or perform any other actions
+                // For example: setInstructor(documents);
+            } catch (error) {
+                console.error('Error getting documents:', error);
+            }
+        };
+
+        // Fetch all documents initially
+        fetchUserData(db, 'Instructor').then(data => {
+            setInstructor(data);
+
+            // After fetching all documents, call the function to get status=false documents
+            getDocumentsWithStatusFalse();
+        });
+
+    }, [db]);
+
+    // Function to handle delete button click
+    const handleDeleteClick = () => {
+       
+    };
+    const handleEditClick = () => {
+      
+    };
+
+
+    return (
+        <>
+            <h4
+                class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300"
+            >
+                Table with actions
+            </h4>
+            <div class="w-full overflow-hidden rounded-lg shadow-xs">
+                <div class="w-full overflow-x-auto">
+                    <table class="w-full whitespace-no-wrap">
+                        <thead>
+                            <tr
+                                class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
+                            >
+                                <th class="px-4 py-3">Name</th>
+                                <th class="px-4 py-3">total course</th>
+                                <th class="px-4 py-3">Status</th>
+                                <th class="px-4 py-3">Date</th>
+                                <th class="px-4 py-3">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody
+                            class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
+                        >
+                            {user?.map((user) => (
+                                <tr class="text-gray-700 dark:text-gray-400">
+                                    <td class="px-4 py-3">
+                                        <div class="flex items-center text-sm">
+                                            {/* <!-- Avatar with inset shadow --> */}
+                                            <div
+                                                class="relative hidden w-8 h-8 mr-3 rounded-full md:block"
+                                            >
+                                                <img
+                                                    class="object-cover w-full h-full rounded-full"
+                                                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                                                    alt=""
+                                                    loading="lazy"
+                                                />
+                                                <div
+                                                    class="absolute inset-0 rounded-full shadow-inner"
+                                                    aria-hidden="true"
+                                                ></div>
+                                            </div>
+                                            <div>
+                                                <p class="font-semibold">{user.fname}</p>
+                                                <p class="text-xs text-gray-600 dark:text-gray-400">
+                                                    {user.experiance}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td class="px-4 py-3 text-sm">
+                                        {user.totalCourse}
+                                    </td>
+                                    <td class="px-4 py-3 text-xs">
+                                    <span
+                                            className={`px-2 py-1 font-semibold leading-tight rounded-full ${user.status
+                                                ? 'text-green-700 bg-green-100 dark:bg-green-700 dark:text-green-100'
+                                                : 'text-red-700 bg-red-100 dark:bg-red-700 dark:text-red-100'
+                                                }`}
+                                        >
+                                            {user.status ? 'approved' : 'not approved'}
+                                        </span>
+                                    </td>
+                                    <td class="px-4 py-3 text-sm">
+                                        6/10/2020
+                                    </td>
+
+
+                                    <td class="px-4 py-3">
+
+                                        <div class="flex items-center space-x-4 text-sm">
+
+                                            <button
+                                                class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                                                aria-label="Edit"
+                                                onClick={() => handleEditClick()}
+                                                key={user.id}
+                                            >
+
+                                                <FaEdit class="w-5 h-5" />
+
+                                            </button>
+
+                                            <button
+                                                class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                                                aria-label="Delete"
+                                                onClick={() => handleDeleteClick()}
+                                                key={user.id}
+
+                                            >
+                                                <MdDeleteOutline class="w-5 h-5" />
+
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+
+
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <div
+                    class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800"
+                >
+                    <span class="flex items-center col-span-3">
+                        Showing 21-30 of 100
+                    </span>
+                    <span class="col-span-2"></span>
+                    {/* <!-- Pagination --> */}
+                    <span class="flex col-span-4 mt-2 sm:mt-auto sm:justify-end">
+                        <nav aria-label="Table navigation">
+                            <ul class="inline-flex items-center">
+                                <li>
+                                    <button
+                                        class="px-3 py-1 rounded-md rounded-l-lg focus:outline-none focus:shadow-outline-purple"
+                                        aria-label="Previous"
+                                    >
+                                        <svg
+                                            class="w-4 h-4 fill-current"
+                                            aria-hidden="true"
+                                            viewBox="0 0 20 20"
+                                        >
+                                            <path
+                                                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                                clip-rule="evenodd"
+                                                fill-rule="evenodd"
+                                            ></path>
+                                        </svg>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button
+                                        class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple"
+                                    >
+                                        1
+                                    </button>
+                                </li>
+                                <li>
+                                    <button
+                                        class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple"
+                                    >
+                                        2
+                                    </button>
+                                </li>
+                                <li>
+                                    <button
+                                        class="px-3 py-1 text-white transition-colors duration-150 bg-purple-600 border border-r-0 border-purple-600 rounded-md focus:outline-none focus:shadow-outline-purple"
+                                    >
+                                        3
+                                    </button>
+                                </li>
+                                <li>
+                                    <button
+                                        class="px-3 py-1 rounded-md rounded-r-lg focus:outline-none focus:shadow-outline-purple"
+                                        aria-label="Next"
+                                    >
+                                        <svg
+                                            class="w-4 h-4 fill-current"
+                                            aria-hidden="true"
+                                            viewBox="0 0 20 20"
+                                        >
+                                            <path
+                                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                                clip-rule="evenodd"
+                                                fill-rule="evenodd"
+                                            ></path>
+                                        </svg>
+                                    </button>
+                                </li>
+                            </ul>
+                        </nav>
+                    </span>
+                </div>
+                {
+                    IsEditOpen &&
+                    <EditModal IsEditOpen={IsEditOpen} setIsEditOpen={setIsEditOpen} user={currentuser} />
+                }
+                <DeleteModal IsDelOpen={IsDelOpen} setIsDelOpen={setIsDelOpen} id={uid} setId={setUid} data={user} setData={user} />
+
+
+            </div>
+
+
+
+        </>
+    )
+}
+
+
+
