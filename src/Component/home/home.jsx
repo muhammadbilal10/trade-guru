9de0a6b3cb08dd2d4bdb9b58d04b16d64f8bfd7c
aquib-javed <aquib.j.basra@gmail.com>
2023-12-13 23:35:11 +0500

    updated landing page
    Co-Authored-By: NIDARAZZAQ <116762229+NIDARAZZAQ@users.noreply.github.com>

--- src/Component/home/home.jsx
+++ src/Component/home/home.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import Navbar from '../navbar/navbar'
-import LoginNavbar from '../navbar/loginnavbar'
+
+import React from 'react';
+import Navbar from '../navbar/navbar';
 
 export default function Home() {
-  return (
-    <div>
-         <Navbar/> 
-        
-      <h1>Home</h1>
-    </div>
-  )
+    return (
+        <div className="bg-gradient-to-r from-purple-600 to-blue-900 min-h-screen flex flex-col text-white">
+            <Navbar />
+
+            <div className="container mx-auto py-10 px-4 animate-fade-in-up">
+                <h1 className="text-5xl font-bold text-center mb-12 animate-pulse">Explore the World of Cryptocurrency</h1>
+                
+                <div className="grid md:grid-cols-2 gap-8">
+                    <div className="transform hover:scale-105 transition duration-500 ease-in-out p-6 bg-purple-500 rounded-lg shadow-xl">
+                        <h2 className="font-bold text-3xl mb-3">Learn Crypto Basics</h2>
+                        <p>Understand the fundamentals of cryptocurrencies and blockchain technology.</p>
+                    </div>
+
+                    <div className="transform hover:scale-105 transition duration-500 ease-in-out p-6 bg-purple-500 rounded-lg shadow-xl">
+                        <h2 className="font-bold text-3xl mb-3">Advanced Trading Strategies</h2>
+                        <p>Explore advanced concepts and trading strategies for cryptocurrency markets.</p>
+                    </div>
+
+                    <div className="transform hover:scale-105 transition duration-500 ease-in-out p-6 bg-purple-500 rounded-lg shadow-xl">
+                        <h2 className="font-bold text-3xl mb-3">Market Analysis</h2>
+                        <p>Analyze cryptocurrency markets for informed investment decisions.</p>
+                    </div>
+
+                    <div className="transform hover:scale-105 transition duration-500 ease-in-out p-6 bg-purple-500 rounded-lg shadow-xl">
+                        <h2 className="font-bold text-3xl mb-3">Community Forum</h2>
+                        <p>Join our forum to discuss and share insights with crypto enthusiasts.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
 }
