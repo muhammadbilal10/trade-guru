db6012aa8db168d9728bc3b1a1f94c43501f5f68
aquib-javed <aquib.j.basra@gmail.com>
2023-12-14 09:54:53 +0500

    navbar
    updated navbar
    
    Co-Authored-By: NIDARAZZAQ <116762229+NIDARAZZAQ@users.noreply.github.com>

--- src/Component/Dashboard/instructor/EditModal.jsx
+++ src/Component/Dashboard/instructor/EditModal.jsx
@@ -26,7 +26,8 @@ function EditModal({ IsEditOpen, setIsEditOpen, user }) {
   }, []);
 
 
-
+  const handleclick = async () => {
+  };
   return (
     <Dialog
       open={IsEditOpen}
@@ -194,8 +195,9 @@ function EditModal({ IsEditOpen, setIsEditOpen, user }) {
             </div> */}
             <button
               type="submit"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Edit</button>
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={() => handleclick()}>
+              Edit</button>
           </form>
 
 
--- src/Component/navbar/navbar.jsx
+++ src/Component/navbar/navbar.jsx
@@ -1,85 +1,35 @@
 import React from "react";
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import TempLogin from "../login/Templogin";
 
 export default function Navbar() {
-
     return (
         <>
-
-            <nav className="navbar bg-[#142941] h-16 text-[#D9ECFF] flex justify-between basis-3">
-                <a href="" className="flex items-center ml-8">
-                    <span >Trade</span>
+            <nav className="navbar bg-gradient-to-r from-purple-600 to-blue-900 h-16 text-white flex justify-between items-center px-8">
+                <a href="/" className="flex items-center space-x-1">
+                    <span>Trade</span>
                     <span className="text-[#21B573]">Guru.pk</span>
                 </a>
-                <div className="flex items-center space-x-6 navbar-links">
-                    <Link to={'/'}>
-                        <button>Home</button>
-                    </Link>
-                    <Link to='/course' >
-                        <button>course</button>
-                    </Link>
-                    <Link to={'/Regesteration'}>
-                        <button>Regesteration</button>
-                    </Link>
-                    <Link to={'/dashboard'}>
-                        <button>Dashboard</button>
-                    </Link>
-                    <button>learn</button>
-                    <button>stocks</button>
-                    <button>Portfolio</button>
+                <div className="flex items-center space-x-6">
+                    <Link to={'/'}><button className="hover:scale-110 transform transition duration-300">Home</button></Link>
+                    <Link to='/course'><button className="hover:scale-110 transform transition duration-300">Course</button></Link>
+                    <Link to={'/Regesteration'}><button className="hover:scale-110 transform transition duration-300">Registration</button></Link>
+                    <Link to={'/dashboard'}><button className="hover:scale-110 transform transition duration-300">Dashboard</button></Link>
+                    <Link to={'/Makepayment'}><button className="hover:scale-110 transform transition duration-300">Make Payemnt</button></Link>
+                    <button className="hover:scale-110 transform transition duration-300">Stocks</button>
+                    <button className="hover:scale-110 transform transition duration-300">Portfolio</button>
                 </div>
-                <div className="flex items-center space-x-4 mr-8 navbar-auth">
-                    {/* {isLoggedIn ? (
-                        // User is logged in, show a welcome message
-                        <div>
-                            <h2>hello ustad g</h2>
-                        </div>
-                    ) : (
-                        // User is not logged in, show login and signup buttons
-                        <div>
-                            <Link to={'/login'}>
-                                <button className="border rounded-md outline-2 w-20 border-[#D9ECFF]" >Login</button>
-                            </Link>
-                            <Link to='/signup'>
-                                <button className="border rounded-md outline-2 w-20  border-[#D9ECFF]">Signup</button>
-                            </Link>
-                        </div>
-                    )} */}
+                <div className="flex items-center space-x-4">
                     <Link to={'/login'}>
-                        <button className="border rounded-md outline-2 w-20 border-[#D9ECFF]" >Login</button>
+                        <button className="border rounded-md px-4 py-1 border-white hover:bg-white hover:text-blue-900 transition duration-300">Login</button>
                     </Link>
                     <Link to='/signup'>
-                        <button className="border rounded-md outline-2 w-20  border-[#D9ECFF]">Signup</button>
+                        <button className="border rounded-md px-4 py-1 border-white hover:bg-white hover:text-blue-900 transition duration-300">Signup</button>
                     </Link>
-                    <Link to={'/instructoform'}>
-                        <button className="border rounded-md outline-2 w-20  border-[#D9ECFF]">become instructor</button>
+                    <Link to={'/instructorform'}>
+                        <button className="border rounded-md px-4 py-1 border-white hover:bg-white hover:text-blue-900 transition duration-300">Become Instructor</button>
                     </Link>
                 </div>
             </nav>
         </>
     );
 }
-
-
-
-
-{/* <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
-                        hello
-                    </a>
-
-           
-
-
-            <div className="bg-[#142941] h-screen">
-               <section className="bg-[#20344A]  w-96 h-96 rounded-lgr">
-                <div className="text-[#D9ECFF] flex flex-col flex items-center">
-                    <h1>login</h1>
-                    <input></input>
-                </div>
-
-
-
-
-*/}
