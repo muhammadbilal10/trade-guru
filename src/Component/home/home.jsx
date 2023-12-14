6804e4bd31f9da8f2abdd41d01a1400d6143d318
aquib-javed <aquib.j.basra@gmail.com>
2023-12-14 09:57:33 +0500

    Update home.jsx
    Co-Authored-By: NIDARAZZAQ <116762229+NIDARAZZAQ@users.noreply.github.com>

--- src/Component/home/home.jsx
+++ src/Component/home/home.jsx
@@ -8,27 +8,27 @@ export default function Home() {
             <Navbar />
 
             <div className="container mx-auto py-10 px-4 animate-fade-in-up">
-                <h1 className="text-5xl font-bold text-center mb-12 animate-pulse">Explore the World of Cryptocurrency</h1>
+                <h1 className="text-5xl font-bold text-center mb-12 animate-pulse">Explore the World of tading</h1>
                 
                 <div className="grid md:grid-cols-2 gap-8">
                     <div className="transform hover:scale-105 transition duration-500 ease-in-out p-6 bg-purple-500 rounded-lg shadow-xl">
-                        <h2 className="font-bold text-3xl mb-3">Learn Crypto Basics</h2>
-                        <p>Understand the fundamentals of cryptocurrencies and blockchain technology.</p>
+                        <h2 className="font-bold text-3xl mb-3">Learn trading Basics</h2>
+                        <p>Understand the fundamentals of technical analysis.</p>
                     </div>
 
                     <div className="transform hover:scale-105 transition duration-500 ease-in-out p-6 bg-purple-500 rounded-lg shadow-xl">
                         <h2 className="font-bold text-3xl mb-3">Advanced Trading Strategies</h2>
-                        <p>Explore advanced concepts and trading strategies for cryptocurrency markets.</p>
+                        <p>Explore advanced concepts and trading strategies for forex markets.</p>
                     </div>
 
                     <div className="transform hover:scale-105 transition duration-500 ease-in-out p-6 bg-purple-500 rounded-lg shadow-xl">
                         <h2 className="font-bold text-3xl mb-3">Market Analysis</h2>
-                        <p>Analyze cryptocurrency markets for informed investment decisions.</p>
+                        <p>Analyze  markets for informed investment decisions.</p>
                     </div>
 
                     <div className="transform hover:scale-105 transition duration-500 ease-in-out p-6 bg-purple-500 rounded-lg shadow-xl">
                         <h2 className="font-bold text-3xl mb-3">Community Forum</h2>
-                        <p>Join our forum to discuss and share insights with crypto enthusiasts.</p>
+                        <p>Join our forum to discuss and share insights with trading enthusiasts.</p>
                     </div>
                 </div>
             </div>
