aaf71e582aafe8e5aab242f9478a90d199ec4acf
NIDARAZZAQ <116762229+NIDARAZZAQ@users.noreply.github.com>
2024-02-18 13:51:12 +0500

    project work

--- src/main.jsx
+++ src/main.jsx
@@ -6,31 +6,31 @@ import "./index.css";
 import "alpinejs";
 import Positions_page from "./Component/Portfolio/temp";
 import Payment from "./Component/payment/payment";
-import Navbar from "./Component/navbar/navbar";
-import Login from "./Component/login/login";
-import Signup from "./Component/signup/signup";
 import Home from "./Component/home/home";
 import CourseForm from "./Component/course/CourseForm";
 import CourseRegistration from "./Component/course/CourseRegistration";
 import UploadResource from "./Component/Resources/UploadResource";
 import TempLogin from "./Component/login/Templogin";
-import TempSignup from "./Component/signup/Tempsignup";
+import Signup from "./Component/signup/signup";
 import DashboardHome from "./Component/Dashboard/home";
 import Instructorform from "./Component/instructor/instructorfoam";
 import Instructorpage from "./Component/Dashboard/instructor/instructorpage";
 import Sidebar from "./Component/sidebar/Sidebar";
 import AddCourseForm from "./Component/course/AddCourseForm";
 import CourseOffer from "./Component/course/CourseOffer";
+import Advertisments from "./Component/Dashboard/advertisement/Advertisement";
 import FileUpload from "./Component/Resources/ResourceUpload";
 import ApproveInstructor from "./Component/Dashboard/instructor/approveInstructor";
 import Terminal from "./Component/terminal/terminal";
 import Portfolio_Main_Page from "./Component/Portfolio/portfolio_main_page";
-import Trade from "./Component/Portfolio/trade_page";
+import MarketSummaryPage from "./Component/Portfolio/MarketSummary";
+
+import OverviewPage from "./Component/Portfolio/OverviewPage";
 const CourseRoutes = () => {
   return (
     <div className="bg-[#eaeef3] min-h-screen flex">
       <div className="flex-1 ml-72">
-        <Routes> 
+        <Routes>
           <Route path="/add" element={<AddCourseForm />} />
           <Route path="/offer" element={<CourseOffer />} />
           <Route path="/uploadResources" element={<FileUpload />} />
@@ -44,30 +44,40 @@ const CourseRoutes = () => {
 const router = createBrowserRouter([
   {
     path: "/",
-    // element: <DashboardHome/>
     element: <Home />,
   },
-  {
-    path: "/dashboard",
-    element: <DashboardHome />,
-  },
-  {
-    path: "/approveinstructor",
-    element: <ApproveInstructor />,
-  },
+  
   {
     path: "/signup",
-    element: <TempSignup />,
-    // element: <Signup/>,
-  },
-  {
-    path: "/instructorpage",
-    element: <Instructorpage />,
+    element: <Signup />,
   },
   {
     path: "/login",
     element: <TempLogin />,
     // element: <Login/>,
+  },
+
+
+  ///dashboard paths
+  {
+    path: "/dashboard",
+    element: <DashboardHome />,
+  },
+
+  //dashboard 
+  //instructor related paths
+  {
+    path: "/approveinstructor",
+    element: <ApproveInstructor />,
+  },
+  {
+    path: "/instructorpage",
+    element: <Instructorpage />,
+  },
+
+  {
+    path: "Advertisment",
+    element: <Advertisments />,
   },
   {
     path: "/course/*",
@@ -79,40 +89,65 @@ const router = createBrowserRouter([
       </>
     ),
   },
+
   {
     path: "/Regesteration",
     element: <CourseRegistration />,
   },
+
   {
     path: "/instructorform",
     element: <Instructorform />,
   },
+
+
+  //terminal folder
   {
     path: "/terminal",
     element: <Terminal />,
   },
-{
-  path:'/portfolio_main_page',
-  element:<Portfolio_Main_Page/>,
-},
 
-{
-  path:'/trade',
-  element:<Trade/>,
-},
-{
-  path:'/positions_page',
-  element:<Positions_page/>,
-},
-{
-  path:'/payment_main_page',
-  element:<Payment/>,
-}
-// ,
-// {
-//   path:'/temp_page',
-//   element:<Temp_page/>,
-// }
+
+
+  //portfolio paths
+  {
+    path: '/portfolio_main_page',
+    element: <Portfolio_Main_Page />,
+  },
+  {
+    path: '/overview_page',
+    element: <OverviewPage />,
+  },
+  {
+    path: '/positions_page',
+    element: <Positions_page />,
+  },
+  {
+    path: '/MarketSummaryPage',
+    element: <MarketSummaryPage />,
+  },
+
+
+
+
+  {
+    path: '/payment_main_page',
+    element: <Payment />,
+  }
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////ZAIN NAZIRS WORK RELATED PATHS/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
