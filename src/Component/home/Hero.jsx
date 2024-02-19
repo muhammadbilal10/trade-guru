ce608e9a287cf518115b0e129d12c8653e0bd8be
muhammadBilal056 <109471675+muhammadBilal056@users.noreply.github.com>
2024-02-19 22:42:50 +0500

    Update Hero Section

--- src/Component/home/Hero.jsx
+++ src/Component/home/Hero.jsx
@@ -5,9 +5,9 @@ const Hero = () => {
   const imageURL =
     "https://bestwpware.com/html/tf/edumim/assets/images/all-img/bred.png";
   return (
-    <div className="my-16 relative">
-      <img src={imageURL} alt="hero" className="w-full" />
-      <h2 className="absolute left-[46%] top-1/2 text-4xl font-bold">
+    <div className="mt-10 mb-24 relative">
+      <img src={imageURL} alt="hero" className="w-full h-96 object-cover" />
+      <h2 className="absolute left-[40%]  sm:left-[46%] top-1/2 text-4xl font-bold">
         Courses
       </h2>
     </div>
--- src/Component/navbar/navbar.jsx
+++ src/Component/navbar/navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="bg-white text-black shadow-lg">
+    <nav className="bg-white text-black shadow-lg h-20">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
